d527134abce2a70b312c5cae6fab30c43c877b98
Seong1-Jo <seong12yada@gmail.com>
2022-06-07 16:30:46 +0900

    feat: Input Tag Focus
    id input tag (useRef, useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
+  const idFocus = useRef("");
+  console.log(idFocus); // #1. id input태그에 current갖고있는 객체를 확인
+
+  useEffect(() => {//#2. 처음 화면이 출력되었을때 행동으로 useEffect를 이용
+    idFocus.current.focus(); //#3. current뒤에다 focus함수를 주면, focus가 주어짐 
+  },[]); //#3. dependency array로 빈값을 주어 처음랜더링 되었을때만 useEffect사용
   return (
     <div>
       <div className='loginId'>
