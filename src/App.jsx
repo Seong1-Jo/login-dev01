43f3693ce7a969acd5f855d45af0060d12a7201f
Seong1-Jo <seong12yada@gmail.com>
2022-06-08 16:29:17 +0900

    feat: Condition(ID,PW) Setting
    id(length over 5, below 10), pw(length over 7)

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,61 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const idFocus = useRef("");
-  console.log(idFocus); // #1. id input태그에 current갖고있는 객체를 확인
+  const idFocus = useRef(""); //id를 위한 focus
+  const pwFocus = useRef(""); //pw를 위한 focus
+  const [ idState, setIdState ] = useState(""); //#5. id,pw의 value를 담을 state를 설정
+  const [ pwState, setPwState ] = useState("");
 
-  useEffect(() => {//#2. 처음 화면이 출력되었을때 행동으로 useEffect를 이용
-    idFocus.current.focus(); //#3. current뒤에다 focus함수를 주면, focus가 주어짐 
-  },[]); //#3. dependency array로 빈값을 주어 처음랜더링 되었을때만 useEffect사용
+  useEffect(() => {
+    idFocus.current.focus(); 
+  },[]); 
+
+  const idChange = (e) => { //#6. onChange하여, id의 value값 업데이트, 같은 방식으로 pw는 jsx에 직접 넣어줌
+    setIdState(e.target.value);
+  };
+ 
+  const checkClick = () => { //#7. onClick를 이용하여 id,pw에 조건에 맞는 alert창 띄우기. 
+    const idLength = idState.length;
+    const pwLength = pwState.length;
+
+    switch (true) {
+      case (idLength >= 5 && idLength <= 10) && (pwLength >= 5):
+        alert('로그인 되었습니다.');
+        break;
+      case (idLength >= 5 && idLength <= 10) && (pwLength < 5):
+        alert('비밀번호를 다시작성해주세요');
+        pwFocus.current.focus();//#8 alert창 후 해당input focus
+        break;
+      default:
+        alert('아이디를 다시작성해주세요');
+        idFocus.current.focus();
+        break;
+    };
+  };
+
   return (
     <div>
       <div className='loginId'>
         <span>ID</span>
-        <input type="text" ref={idFocus}/>
+        <input 
+        type="text" 
+        ref={idFocus} 
+        onChange={idChange} 
+        value={idState} 
+        />
       </div>
       <div className="loginPw">
         <span>PW</span>
-        <input type="password" />
+        <input 
+        type="password"
+        ref={pwFocus}  
+        onChange={ e =>  setPwState(e.target.value) } 
+        value={pwState} 
+        />  
       </div>
       <div className="loginBtn">
-        <button>Login</button>
+        <button onClick={checkClick}>Login</button>
       </div>
     </div>
   )
